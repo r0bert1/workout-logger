4f18677471d9a235cd3203ac681f7ee6504b806d
r0bert1 <rpakkane@gmail.com>
2020-03-26 13:17:41 +0200

    added login and register functionality

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
+  
+    class Meta:
+        csrf = False
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", [validators.Length(min=2)])
+    password = PasswordField("Password", [validators.Length(min=5)])
   
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,8 +1,11 @@
 from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
+
+from sqlalchemy.exc import IntegrityError
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -10,7 +13,6 @@ def auth_login():
         return render_template("auth/loginform.html", form = LoginForm())
 
     form = LoginForm(request.form)
-    # mahdolliset validoinnit
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
@@ -18,5 +20,33 @@ def auth_login():
                                error = "No such username or password")
 
 
-    print("Käyttäjä " + user.username + " tunnistettiin")
+    login_user(user)
     return redirect(url_for("index"))
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form = RegisterForm())
+    
+    form = RegisterForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/registerform.html", form = form)
+
+    user = User(form.username.data, form.password.data)
+    
+    try:
+        db.session().add(user)
+        db.session().commit()
+    except IntegrityError:
+        return render_template("auth/registerform.html", form = form,
+                                error = "Username " + user.username + " already exists")
+        
+
+    return redirect(url_for("auth_login"))
+
