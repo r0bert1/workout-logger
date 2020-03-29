32a977a8cb4cb8d98a00c1a24710764263443504
r0bert1 <rpakkane@gmail.com>
2020-03-30 00:56:56 +0300

    added react build to git for heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: cd react-ui && npm install && npm run build
 web: gunicorn --preload --workers 1 application:app
---
+++ react-ui/build/precache-manifest.ca6a1041e563cc419008a77a395b360d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3818147f10e336ec94cb1191601edaca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508e880db1327d00c52c",
+    "url": "/static/js/2.0420a6d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0420a6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef02d517dfdb4b09393b",
+    "url": "/static/js/main.466e3839.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
