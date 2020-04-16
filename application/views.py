899d6941faa2e8d4532bb99e29b87e0ac3ff7daa
r0bert1 <rpakkane@gmail.com>
2020-04-16 18:13:26 +0300

    fixed catch all not returning build files

--- application/views.py
+++ application/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from application import app
 import os
 
@@ -8,13 +8,17 @@ import os
 # def index():
 #    return render_template("index.html")
 
-# Serve react files
-
 def bundle_last_updated():
     return str(os.path.getmtime('./react-ui/build/bundle.js'))
+
+# Serve react files
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<string:path>')
 @app.route('/<path:path>')
 def catch_all(path):
+    path_dir = os.path.abspath('./react-ui/build')
+    if path != "" and os.path.exists(os.path.join(path_dir, path)):
+        return send_from_directory(os.path.join(path_dir), path)
+
     return render_template('react-index.html', last_updated=bundle_last_updated())
