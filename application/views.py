91c5469b28e00a62aa627cdf0fd53e6c9a05e544
r0bert1 <rpakkane@gmail.com>
2020-04-16 17:11:41 +0300

    added catch all route

--- application/views.py
+++ application/views.py
@@ -8,11 +8,13 @@ import os
 # def index():
 #    return render_template("index.html")
 
+# Serve react files
+
 def bundle_last_updated():
     return str(os.path.getmtime('./react-ui/build/bundle.js'))
 
-# Serve react files
-
-@app.route("/")
-def react_index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<string:path>')
+@app.route('/<path:path>')
+def catch_all(path):
     return render_template('react-index.html', last_updated=bundle_last_updated())
