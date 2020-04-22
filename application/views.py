023f1a63404b7e15d4751e3471e897971ccef150
r0bert1 <rpakkane@gmail.com>
2020-04-22 19:53:05 +0300

    fixed catch-all not supporting nested urls

--- application/views.py
+++ application/views.py
@@ -13,12 +13,19 @@ def bundle_last_updated():
 
 # Serve react files
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<string:path>')
-@app.route('/<path:path>')
-def catch_all(path):
+# @app.route('/', defaults={'path': ''})
+@app.route('/', defaults={'path1': '', 'path2': ''})
+@app.route('/<string:path1>', defaults={'path2': ''})
+@app.route('/<string:path1>/<string:path2>')
+def catch_all(path1, path2):
+    print(path2)
+    print(path1)
     path_dir = os.path.abspath('./react-ui/build')
-    if path != "" and os.path.exists(os.path.join(path_dir, path)):
-        return send_from_directory(os.path.join(path_dir), path)
+
+    if path1 != '' and path2 == '' and os.path.exists(os.path.join(path_dir, path1)):
+        return send_from_directory(os.path.join(path_dir), path1)
+
+    if path1 != '' and path2 != '' and os.path.exists(os.path.join(path_dir, path2)): 
+        return send_from_directory(os.path.join(path_dir), path2)
 
     return render_template('react-index.html', last_updated=bundle_last_updated())
